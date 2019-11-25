6767b62d7edae7b66390e4f236b29b265334d3cf
jattoabdul <jattoade@gmail.com>
2019-11-25 01:46:58 +0100

    adding hotel logo and style limehome component

--- components/limehome.tsx
+++ components/limehome.tsx
@@ -20,8 +20,7 @@ const LimeHome = (props: any) => {
 
   const mapStyles = {height: '100vh', width: '50vw',};
 
-  // TODO: tune properly to get accurate current location
-  const { latitude, longitude, error } = usePosition(true, {watch: true, enableHighAccuracy: true});
+  const { latitude, longitude, error } = usePosition(true, {watch: true, enableHighAccuracy: true, timeout: 20000, maximumAge: 1000});
   let mapCenter;
   if (location.value === '' && latitude && longitude && !error) {
     onChangeLocation({
@@ -40,38 +39,45 @@ const LimeHome = (props: any) => {
   return (
     <React.Fragment>
       <div className="app-wrapper">
-        <div>
-          <h1>LimeHome</h1>
-          <form onSubmit={(e) => {
-            e.preventDefault();
-            setHotels({
-              apiKey: publicRuntimeConfig.gApiKey,
-              location:  location.value,
-              proximity: proximity.value,
-              opennow: true
-            });
-          }}>
-            <input
-              type="text"
-              value={location.value}
-              onChange={e => onChangeLocation({
-                value: e.target.value,
-              })}
-            />
-            <br />
-            <input
-              type="submit"
-              value="Locate Nearby Hotels"
-            />
-          </form>
-          {/* proximity - input field */}
-          {data.map((hotel, index) => (
-            <p key={index}>
-              {hotel.name}
-            </p>
-          ))}
+        <div className="list-wrapper">
+          <div className="res">
+            <div className="header-wrapper">
+              <img src="/hotel.svg" alt="logo"/>
+              <div className="hambuger" />
+            </div>
+            <form onSubmit={(e) => {
+              e.preventDefault();
+              setHotels({
+                apiKey: publicRuntimeConfig.gApiKey,
+                location:  location.value,
+                proximity: proximity.value,
+                opennow: true
+              });
+            }}>
+              <input
+                type="search"
+                value={location.value}
+                onChange={e => onChangeLocation({
+                  value: e.target.value,
+                })}
+              />
+              <input
+                type="submit"
+                value="Locate Nearby Hotels"
+              />
+            </form>
+          </div>
+          <div className="hotel-result-list">
+            {data.map((hotel, index) => (
+              <div className="hotel-result-item" key={index}>
+                <img src="https://d19yo8val8huli.cloudfront.net/hotels/v7/img/featured/exquisite-suites.jpg" alt="hotel img"/>
+                <h2>{hotel.name}</h2>
+                <p>{hotel.vicinity}</p>
+              </div>
+            ))}
+          </div>
         </div>
-        <div>
+        <div className="map-wrapper">
           <SimpleMap mapStyles={mapStyles} locations={data} mapCenter={mapCenter} zoomRate={13} />
         </div>
       </div>
